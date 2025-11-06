09af56d4f41cc9cacb0e4c5b2ea01094ba8dad80
TraDkhan <trandkhanh04@gmail.com>
2025-11-06 13:56:53 +0700

    hoan thanh ui mo khoa tung level

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Level
+public class LevelManager : MonoBehaviour
 {
-    internal class LevelManager
-    {
-        void OnLevelComplete(int levelId, int score)
-        {
-            var data = GameDataManager.Instance.currentData;
-            data.UnlockLevel(levelId + 1);     // Mở khóa màn kế tiếp
-            data.UpdateHighScore(levelId, score); // Lưu điểm cao nhất
-            GameDataManager.Instance.SaveGame();
-        }
+    public GameObject[] levels; // Gán các level trong Inspector
 
+    private void Start()
+    {
+        int selectedLevel = PlayerPrefs.GetInt("SelectedLevel", 0);
+
+        // Vô hiệu hóa tất cả level
+        foreach (GameObject lvl in levels)
+            lvl.SetActive(false);
+
+        // Bật level được chọn
+        if (selectedLevel >= 0 && selectedLevel < levels.Length)
+            levels[selectedLevel].SetActive(true);
+        else
+            Debug.LogWarning("SelectedLevel out of range!");
+    }
+
+    void OnLevelComplete(int levelId, int score)
+    {
+        var data = GameDataManager.Instance.currentData;
+        data.UnlockLevel(levelId + 1);     // Mở khóa màn kế tiếp
+        data.UpdateHighScore(levelId, score); // Lưu điểm cao nhất
+        GameDataManager.Instance.SaveGame();
     }
 }
---
+++ Assets/Scripts/Level/LevelSelectButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public int levelIndex; // Số thứ tự level (0, 1, 2, ...)
+    public Image lockIcon; // Sprite hình khóa
+    public Button button;  // Nút button thực tế
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", 6);
+        PlayerPrefs.Save();
+        // Kiểm tra xem level này đã được mở khóa chưa
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 0);
+
+        bool isUnlocked = levelIndex <= unlockedLevel;
+
+        lockIcon.gameObject.SetActive(!isUnlocked); // Hiện khóa nếu chưa mở
+        button.interactable = isUnlocked;           // Tắt bấm nếu chưa mở
+        if (isUnlocked)
+        {
+            button.onClick.AddListener(() => OnSelectLevel());
+        }
+    }
+
+    public void OnSelectLevel()
+    {
+        PlayerPrefs.SetInt("SelectedLevel", levelIndex);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("SampleScene");
+    }
+}
